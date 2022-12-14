4e582e62b4b52db0f4b455b33cfbe9399cda4262
MDimas27 <mdimas.umb@gmail.com>
2022-12-14 15:45:16 +0700

    Update section 7 - create object pattern

--- cypress/e2e/step_definitions/loginStep.js
+++ cypress/e2e/step_definitions/loginStep.js
@@ -1,16 +1,20 @@
 import { Given, Then, When} from "@badeball/cypress-cucumber-preprocessor";
+import LoginPage from "./loginPage";
 
 Given("I open login page", () => {
-    cy.visit("http://zero.webappsecurity.com/login.html")
+    // cy.visit("http://zero.webappsecurity.com/login.html")
+    LoginPage.visit()
 })
 
 When("I submit login", () => {
-    cy.fixture("user").then(user => {
-        const username = user.username
-        const password = user.password
+    // cy.fixture("user").then(user => {
+    //     const username = user.username
+    //     const password = user.password
 
-        cy.loginStep(username, password)
-    })
+    //     cy.loginStep(username, password)
+    // })
+    LoginPage.fillUsernameAndPassword('username', 'password')
+    LoginPage.signIn()
 })
 
 Then('I should see homepage', () => {
